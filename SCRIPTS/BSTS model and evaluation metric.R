91adbb132026a645c280d12e979f1c356a7be4cc
borayd3 <143008492+borayd3@users.noreply.github.com>
2025-04-29 00:06:34 -0400

    Update and rename BSTS MODEL.R to BSTS model and evaluation metric.R

--- SCRIPTS/BSTS model and evaluation metric.R
+++ SCRIPTS/BSTS model and evaluation metric.R
@@ -71,7 +71,35 @@ ggplot(forecast_df, aes(x = datetime, y = predicted_ridership)) +
   ggtitle("BSTS Forecast for October 30th, 2023") +
   xlab("Hour") + ylab("Predicted Ride Count")
 
+#### BSTS Evaluations
 
+# Actual values from the test data
+actual_values <- test_data$ride_count[1:horizon]
+
+# Ensure that bsts_pred is numeric before evaluating
+if (!is.numeric(forecast_values)) {
+  stop("bsts_pred is not numeric.")
+}
+
+# Evaluate RMSE, MAE, MAPE
+rmse_bsts <- sqrt(mean((actual_values - forecast_values)^2))
+mae_bsts <- mean(abs(actual_values - forecast_values))
+mape_bsts <- mean(abs((actual_values - forecast_values) / actual_values)) * 100
+
+
+
+# Print metrics
+print(rmse_bsts)
+print(mae_bsts)
+print(mape_bsts)
+
+> # Print metrics
+  > print(rmse_bsts)
+[1] 25.76266
+> print(mae_bsts)
+[1] 18.78601
+> print(mape_bsts)
+[1] 11.84987
 
 
 
